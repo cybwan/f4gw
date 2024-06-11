e288cb363efae79b898eb4fda8cc9875512898bc
Cybwan <baili@flomesh.io>
2024-06-11 13:16:05 +0800

    init

--- pkg/bpf/proxy/proxy.go
+++ pkg/bpf/proxy/proxy.go
@@ -1,6 +1,10 @@
 package proxy
 
 import (
+	"fmt"
+	"net"
+
+	"github.com/cybwan/f4gw/pkg/bpf"
 	"github.com/cybwan/f4gw/pkg/libbpf"
 )
 
@@ -9,25 +13,33 @@ func (proxy *F4Proxy) Init() {
 		log.Fatal().Msgf("remove memlock error: %v", err)
 	}
 
+	proxy.prog = "proxy"
+	proxy.progFile = fmt.Sprintf("%s.kern", proxy.prog)
+
 	proxy.cleanCallbacks = make(map[string]func() error)
-	//proxy.bpfObjs = new(bpfObjects)
-	//
-	//opts := ebpf.CollectionOptions{}
-	//opts.Maps.PinPath = bpf.BPF_FS
-	//if err := loadBpfObjects(proxy.bpfObjs, &opts); err != nil {
-	//	log.Fatal().Err(err).Msg(`loading bpf objects`)
-	//}
-	//
-	//proxy.cleanCallbacks[`close bpf Objects`] = proxy.bpfObjs.Close
-	//proxy.cleanCallbacks[`clean bpf Maps`] = func() error {
-	//	os.Remove(fmt.Sprintf(`%s/%s`, bpf.BPF_FS, `f4gw_nat_opts`))
-	//	os.Remove(fmt.Sprintf(`%s/%s`, bpf.BPF_FS, `f4gw_egr_ipv4`))
-	//	os.Remove(fmt.Sprintf(`%s/%s`, bpf.BPF_FS, `f4gw_igr_ipv4`))
-	//	os.Remove(fmt.Sprintf(`%s/%s`, bpf.BPF_FS, `f4gw_fc_v4`))
-	//	os.Remove(fmt.Sprintf(`%s/%s`, bpf.BPF_FS, `f4gw_nat`))
-	//	os.Remove(fmt.Sprintf(`%s/%s`, bpf.BPF_FS, `f4gw_progs`))
-	//	return nil
-	//}
+
+	if !libbpf.IsBpfFS(bpf.BPF_FS) {
+		success, err := libbpf.MountBpfFS(bpf.BPF_FS)
+		if err != nil {
+			log.Fatal().Err(err).Msg(`mount bpf file system`)
+		}
+		if !success {
+			log.Fatal().Msg(`mount bpf file system error`)
+		}
+	}
+
+	if err := libbpf.UnloadAll(bpf.BPF_FS, proxy.prog); err != nil {
+		log.Fatal().Err(err).Msg(`unloading bpf objects`)
+	}
+
+	if err := libbpf.LoadAll(bpf.BPF_FS, proxy.prog, proxy.progFile); err != nil {
+		log.Fatal().Err(err).Msg(`loading bpf objects`)
+	}
+
+	proxy.cleanCallbacks[`clean bpf Objects`] = func() error {
+		libbpf.UnloadAll(bpf.BPF_FS, proxy.prog)
+		return nil
+	}
 }
 
 func (proxy *F4Proxy) Close() {
@@ -40,23 +52,23 @@ func (proxy *F4Proxy) Close() {
 }
 
 func (proxy *F4Proxy) AttachIngressBPF(iface string) {
-	//// Look up the network interface by name.
-	//ingressIface, ingressErr := net.InterfaceByName(iface)
-	//if ingressErr != nil {
-	//	log.Error().Msgf("lookup network ingress iface %s: %s", iface, ingressErr)
-	//	return
-	//}
-	//// Attach the program.
-	//ingressLink, ingressErr := link.AttachXDP(link.XDPOptions{
-	//	Program:   proxy.bpfObjs.XdpIngress,
-	//	Interface: ingressIface.Index,
-	//})
-	//if ingressErr != nil {
-	//	log.Error().Msgf("could not attach XDP program: %s", ingressErr)
-	//	return
-	//}
-	//
-	//proxy.cleanCallbacks[fmt.Sprintf("Detached XDP program to ingress iface %q (index %d)", ingressIface.Name, ingressIface.Index)] = ingressLink.Close
-	//
-	//log.Info().Msgf("Attached XDP program to ingress iface %q (index %d)", ingressIface.Name, ingressIface.Index)
+	// Look up the network interface by name.
+	ingressIface, ingressErr := net.InterfaceByName(iface)
+	if ingressErr != nil {
+		log.Error().Msgf("lookup network ingress iface %s: %s", iface, ingressErr)
+		return
+	}
+
+	ingressErr = libbpf.AttachXDP(iface, fmt.Sprintf("%s/%s/%s", bpf.BPF_FS, proxy.prog, "xdp_ingress"))
+	if ingressErr != nil {
+		log.Fatal().Msgf("could not attach XDP program: %s", ingressErr)
+		return
+	}
+
+	proxy.cleanCallbacks[fmt.Sprintf("Detached XDP program to ingress iface %q (index %d)", ingressIface.Name, ingressIface.Index)] = func() error {
+		libbpf.DetachXDP(iface)
+		return nil
+	}
+
+	log.Info().Msgf("Attached XDP program to ingress iface %q (index %d)", ingressIface.Name, ingressIface.Index)
 }
